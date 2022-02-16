c98258aceaf7769c4b073cddf9a9c72a45989d98
Laurafarrerdev <laurafarrer1@gmail.com>
2022-02-16 12:49:57 +0100

    controller e interfaz repository

---
+++ src/main/java/controller/ProductController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Product;
+
+@RestController
+@RequestMapping("/products")
+@CrossOrigin
+
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public String add(@RequestBody Product product) {
+        productService.saveProduct(product);
+        return "new product is added";
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+}
